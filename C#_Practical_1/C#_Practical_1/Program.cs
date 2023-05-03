1465f5f52f748e54b7245b7a0ab3d47a19ca0f42
Abhi Patel <abhi.d@simformsolutions.com>
2023-05-03 13:29:44 +0530

    add : Program.cs file
    main method and logic is added to this file.

---
+++ C#_Practical_1/C#_Practical_1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Calculator
+{
+	internal class Program
+	{
+		#region Function Form outside
+		// Getting the functions form outside a program
+		[DllImport("kernel32.dll", ExactSpelling = true)]
+		private static extern IntPtr GetConsoleWindow();
+		private static IntPtr ThisConsole = GetConsoleWindow();
+
+		[DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+		private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+		private const int MAXIMIZE = 3;
+
+		#endregion
+
+
+		// maximizing the console
+		static Program()
+		{
+			Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+			ShowWindow(ThisConsole, MAXIMIZE);
+		}
+
+		static void Main(string[] args)
+		{
+			Numbers userInput = InputClass.TakeNumbersFromUser();
+			UIClass.PrintResult(CalculateDataFromGivenData(userInput), userInput);
+
+			//int a = 10, b = 0;
+   //         Console.WriteLine(a/b);
+        }
+
+		public static CalculatorResultData CalculateDataFromGivenData(Numbers numbers)
+		{
+			double x = numbers.number1;
+			double y = numbers.number2;
+
+			return new CalculatorResultData(x+y,x-y,x*y,Math.Round((x/y),4));
+		}
+	}
+}
